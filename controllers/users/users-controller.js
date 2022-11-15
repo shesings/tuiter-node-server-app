707e4799a24e98ad578af6370d16aae6ce0e3927
Sheetal <sheetalsingh7007@gmail.com>
2022-11-14 19:14:03 -0600

    3.2 Sending query parameters to a Web Server

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -3,8 +3,15 @@ let users = people
 const UserController = (app) => {       // use express instance app to declare HTTP GET
     app.get('/api/users', findUsers)    // request pattern /api/users to call a function
 }
-const findUsers = (req, res) => {       // function runs when /api/users requested
-    res.json(users)                     // responds with array of users
+const findUsers = (req, res) => {
+    const type = req.query.type             // retrieve type parameter from query
+    if(type) {                              // if type parameter in query
+        const usersOfType = users           // find users of that type
+            .filter(u => u.type === type)
+        res.json(usersOfType)               // respond with users of that type
+        return                              // return so it doesn't continue
+    }
+    res.json(users)                         // otherwise respond with all users
 }
 export default UserController           // exports so app.js can import
 

3f9b1cfd1a3045d658dee2979a69836ae751c110
Sheetal <sheetalsingh7007@gmail.com>
2022-11-15 13:45:54 -0600

    5.1 Retrieving data from a RESTful Web service API

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,21 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime() + '';
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+const findTuits = (req, res) => {
+    res.json(tuits);
+}
+const updateTuit = (req, res) => {}
+const deleteTuit = (req, res) => {}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}

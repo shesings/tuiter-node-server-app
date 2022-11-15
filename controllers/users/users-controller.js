ce5a6d9144ba554d1598738525de09a001940975
Sheetal <sheetalsingh7007@gmail.com>
2022-11-14 19:17:45 -0600

    3.3 Sending path parameters to a Web Server

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -1,7 +1,8 @@
 import people from './users.js'         // import the array of users
 let users = people
 const UserController = (app) => {       // use express instance app to declare HTTP GET
-    app.get('/api/users', findUsers)    // request pattern /api/users to call a function
+    app.get('/api/users', findUsers);    // request pattern /api/users to call a function
+    app.get('/api/users/:uid', findUserById);
 }
 const findUsers = (req, res) => {
     const type = req.query.type             // retrieve type parameter from query
@@ -12,6 +13,12 @@ const findUsers = (req, res) => {
         return                              // return so it doesn't continue
     }
     res.json(users)                         // otherwise respond with all users
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users.find(u => u._id === userId);
+    res.json(user);
 }
 export default UserController           // exports so app.js can import
 

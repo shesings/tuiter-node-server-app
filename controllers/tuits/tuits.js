d688c8d80dc93e58caa5c04932572b36d67ffd26
Sheetal <sheetalsingh7007@gmail.com>
2022-11-20 19:40:29 -0500

    add dislikes for existing models

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -10,6 +10,7 @@ export default [
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true,
         "likes": 2345,
+        "dislikes": 121,
         "replies": 123,
         "retuits": 432
 },
@@ -24,6 +25,7 @@ export default [
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
     "liked": true,
     "likes": 2345,
+    "dislikes": 121,
     "replies": 123,
     "retuits": 432
 },

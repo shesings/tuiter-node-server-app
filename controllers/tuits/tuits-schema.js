1c128bd8f40678794df2126ff5b7483cd707156b
Sheetal <sheetalsingh7007@gmail.com>
2022-11-23 17:26:17 -0600

    Bug Fix: Whenever anew tweet is made, the dislike would produce NaN

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
+// Note: Whenever a new Tuit is created or new data fields needs to be added in the DB,
+// This is where you want to add new fields for the DB to be able to read and understand
 const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    dislikes: Number,
 }, {collection: 'tuits'});
 export default schema;

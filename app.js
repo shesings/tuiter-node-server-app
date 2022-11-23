f271fd8450eb529eff355cc59d99b94bb635a2a5
Sheetal <sheetalsingh7007@gmail.com>
2022-11-23 05:56:18 -0600

    4.2 integrating node js with mongo DB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";                            // load the mongoose library
-mongoose.connect('mongodb://localhost:27017/tuiter');    // connect to the tuiter database
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 import express from 'express';
 import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
